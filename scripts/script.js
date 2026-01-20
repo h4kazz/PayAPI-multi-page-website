e5dcc4fd7557415ed5aa8f5ca01d03a4f98088f9
berzjar <berzjar@gmail.com>
2026-01-20 19:52:47 +0200

    Release v.02

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,15 @@
 
 const menuToggle = document.getElementById('menu-toggle');
 const sideMenu = document.getElementById('side-menu');
+const closeMenu = document.getElementById('close-menu');
 
 menuToggle.addEventListener('click', () => {
-    sideMenu.classList.toggle('open');
+    sideMenu.classList.add('open');
 });
+
+closeMenu.addEventListener('click', () => {
+    sideMenu.classList.remove('open');
+});
+
 
 //  Header Menu End
